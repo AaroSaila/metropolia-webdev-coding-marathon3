850b68c149fe9848bba031d6b4a46c49367c64ea
oiivantsov <oiivantsov@outlook.com>
2024-12-04 15:10:55 +0200

    useField to accept files

--- frontend/src/hooks/useField.jsx
+++ frontend/src/hooks/useField.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-export default function useField(type) {
+export default function useField(type, required = false, accept = "") {
   const [value, setValue] = useState("");
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
-  return { type, value, onChange };
+  const fieldProps = { type, value, onChange, required }
+
+if (type === "file" && accept) {
+  fieldProps.accept = accept;
+}
+
+return fieldProps;
 };
